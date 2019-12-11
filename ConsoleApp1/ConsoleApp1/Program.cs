7df6ca26cdce0401890a910a3729e61cfdb68026
Diego Gomes dos Santos Zaratine <diego.zaratine@kroton.com.br>
2019-12-11 15:00:56 -0300

    texto

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello WorldER!");
+            Console.WriteLine("Hello WorldfghfghfghgfER!");
         }
     }
 }
